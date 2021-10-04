fa42a98c494792c62002d003b44e42e5d93e5fa6
Wan Arif <54633167+0ne4rif@users.noreply.github.com>
2021-10-04 21:25:53 +0800

    Added batch file

---
+++ auto.bat
@@ -0,0 +1,7 @@
+title Runner
+echo Executing background task...
+cd "C:\Program Files\Affinity\Designer\"
+start Designer.exe
+cd "C:\Program Files\Blender Foundation\Blender 2.93\"
+start blender.exe
+exit
